d4f9ce33eec487ac9bb72ae710a88d8109666b59
jianjustin <janine.jian.chen@gmail.com>
2020-06-01 12:33:39 +0800

    change service url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$http = axios
-axios.defaults.baseURL = 'http://127.0.0.1:8081/'
+axios.defaults.baseURL = 'https://shadow-authorization-server.herokuapp.com/'
 axios.defaults.headers.get['Accept'] = 'application/json'
 
 const token = localStorage.getItem('token')
