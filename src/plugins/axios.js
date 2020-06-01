48456e1624953be5e4eef32047f80690f5585af6
jianjustin <janine.jian.chen@gmail.com>
2020-06-01 15:35:02 +0800

    change service url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import axios from 'axios'
 
 Vue.prototype.$http = axios
-axios.defaults.baseURL = 'https://shadow-authorization-server.herokuapp.com/'
+axios.defaults.baseURL = 'http://localhost:8081/'
 axios.defaults.headers.get['Accept'] = 'application/json'
 
 const token = localStorage.getItem('token')
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,7 +4,6 @@ import theme from './theme'
 import 'vuetify/dist/vuetify.min.css'
 import '@mdi/font/css/materialdesignicons.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
-// This is here just in case you prefer to have all your plugins in a folder like this, otherwise its currently unused
 
 Vue.use(Vuetify, {
   iconfont: 'mdi',
