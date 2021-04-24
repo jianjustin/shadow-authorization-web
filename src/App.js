f8ca84e0fc0242f3f85fb45f3469d14ad2d816b4
chenjianrui <janine.jian.chen@gmail.com>
2021-04-24 13:06:18 +0800

    添加material-ui依赖

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Button from '@material-ui/core/Button';
 
 function App(){
   return (
     <div className="App">
-      <h1> Hello, World! </h1>
+      <Button variant="contained" color="primary">Hello, World! </Button>
     </div>
   );
 }
